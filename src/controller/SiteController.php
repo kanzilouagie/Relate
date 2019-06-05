7495c9e5af98ecc2addaa56f6d6cb6ff1f724523
Kanzi Louagie <kanzi.louagie@student.howest.be>
2019-06-05 16:24:19 +0200

    init project relate

---
+++ src/controller/SiteController.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once __DIR__ . '/Controller.php';
+
+
+class SiteController extends Controller{
+
+  function __construct(){
+  }
+
+  public function index(){
+    $this->set('title','Vinyl and Chill');
+  }
+
+}
---
+++ src/view/layout.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="nl">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://fonts.googleapis.com/css?family=Ubuntu" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js"></script>
+  <script>
+    WebFont.load({
+      custom: {
+        families: ["Longreach Regular"],
+        urls: ["assets/fonts/fonts.css"]
+      }
+    });
+
+  </script>
+  <title><?php echo($title);   ?></title>
+  <?php echo($css);  ?>
+</head>
+<body>
+  <?php  echo($content);  ?>
+  <?php echo($js) ?>
+</body>
+</html>
