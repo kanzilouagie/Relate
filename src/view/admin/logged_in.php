918ad69447d6366bd41e4f694bb6aab649d90340
XavierAllen <xavier.allen@student.howest.be>
2019-06-07 16:09:32 +0200

    Styling home

--- src/view/admin/logged_in.php
+++ src/view/admin/logged_in.php
@@ -4,8 +4,15 @@
     <h2>Hierna moet u zich weer aanmelden.</h2>
     <a class="submitform" href="index.php?page=logged_in&actie=loguit">log out</a>
   </section>
-<form method="POST">
-  <input type="text" name="username" placeholder="username" required />
-  <input type="password" name="password" placeholder="password" required>
-  <input type='submit' value='register' name='action' />
-</form>
+
+<section>
+
+</section>
+
+<div class= "login-form">
+  <form method="POST">
+    <input type="text" name="username" placeholder="username" required />
+    <input type="password" name="password" placeholder="password" required>
+    <input class="sb" type='submit' value='register' name='action' />
+  </form>
+</div>
--- src/view/site/__navigation.php
+++ src/view/site/__navigation.php
@@ -1,8 +1,9 @@
 <header>
-  <nav>
+  <div class="navigation_logo">test</div>
+  <nav class="navigation">
     <a href="index.php?page=home">home</a>
     <a href="index.php?page=verhalen">verhalen</a>
     <a href="index.php?page=twotales">TwoTales</a>
-
+    <a href="index.php?page=admin">admin</a>
   </nav>
 </header>
