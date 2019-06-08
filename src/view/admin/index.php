3a498672c8d37fd38d5039f7689644a0f98aa335
XavierAllen <xavier.allen@student.howest.be>
2019-06-08 13:58:05 +0200

    Homepage fold design

--- src/view/admin/index.php
+++ src/view/admin/index.php
@@ -1,17 +1,22 @@
-<?php
-  if(isset($_POST['action'])){
-    if($_POST['action'] === 'login') {
-  if($_SESSION['logged_in'] === false) { ?>
-  <div class="errors">
-    <p>Wachtwoord en/of login is incorrect.</p>
-  </div>
-  <?php }}}; ?>
 
-<div class= "login-form">
-  <form class="form_login__form" method='POST' action=''>
-    <input class='inputfield' type='text' name='username' placeholder='Gebruikersnaam' required /><br /><br />
-    <input class='inputfield' type='password' name='password' placeholder='Wachtwoord' required /><br /><br />
-    <input class='sb' type='submit' value='login' name='action' />
-  </form>
-  <a href=""><p>Forgot your password?</p></a>
+<div class="admin_login_body" >
+  <?php
+    if(isset($_POST['action'])){
+      if($_POST['action'] === 'login') {
+    if($_SESSION['logged_in'] === false) { ?>
+    <div class="errors">
+      <p>Wachtwoord en/of login is incorrect.</p>
+    </div>
+    <?php }}}; ?>
+  <div class= "login_wrapper">
+    <div class= "login-form">
+      <form class="form_login__form" method='POST' action=''>
+        <input class='inputfield' type='text' name='username' placeholder='Gebruikersnaam' required /><br /><br />
+        <input class='inputfield' type='password' name='password' placeholder='Wachtwoord' required /><br /><br />
+        <input class='sb' type='submit' value='login' name='action' />
+      </form>
+      <a href=""><p>Forgot your password?</p></a>
+    </div>
+  </div>
+
 </div>
--- src/view/site/index.php
+++ src/view/site/index.php
@@ -1,6 +1,13 @@
 <?php include '__navigation.php' ?>
 
-<div class="home">
+<section class="fold">
+  <div class="fold_black">
+    <img class="fold_black_img" src="./assets/images/header_Home.png" alt="" >
+  </div>
 
-</div>
+  <div class="fold_red">
+  <img class="fold_red_img" src="./assets/images/header_Home2.png" alt="" >
+  </div>
+</section>
+
 
