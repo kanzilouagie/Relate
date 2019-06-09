527cfa159870fb849a01617bcacf426df8523105
Kanzi Louagie <kanzi.louagie@student.howest.be>
2019-06-09 21:07:20 +0200

    werken aan de responsiveness

--- src/index.js
+++ src/index.js
@@ -14,9 +14,35 @@ function toggle() {
     $menu.style.display = 'block';
     $top.style.marginLeft = '30rem';
     $main.style.marginLeft = '30rem';
+    $top.style.width = 'calc(100% - 30rem)';
+    $main.style.width = 'calc(100% - 30rem)';
   } else {
     $menu.style.display = 'none';
     $top.style.marginLeft = '0';
     $main.style.marginLeft = '0';
+    $top.style.width = '100%';
+    $main.style.width = '100%';
+  }
+}
+
+document.getElementsByTagName('body')[0].onresize = function() { myFunction(); };
+function myFunction() {
+  const $menu = document.querySelector('.admin_header');
+  const $top = document.querySelector('.admin__top_header');
+  const $main = document.querySelector('.dashboard_main');
+  const w = window.innerWidth;
+
+  if (w >= 1030) {
+    $menu.style.display = 'block';
+    $top.style.marginLeft = '30rem';
+    $main.style.marginLeft = '30rem';
+    $top.style.width = 'calc(100% - 30rem)';
+    $main.style.width = 'calc(100% - 30rem)';
+  } else {
+    $menu.style.display = 'none';
+    $top.style.marginLeft = '0';
+    $main.style.marginLeft = '0';
+    $top.style.width = '100%';
+    $main.style.width = '100%';
   }
 }
