9d466c9055f9a03ffe976a3e26716f2966c73d4f
XavierAllen <xavier.allen@student.howest.be>
2019-06-10 17:56:20 +0200

    twotales functionaliteit fix

--- src/view/site/index.php
+++ src/view/site/index.php
@@ -1,4 +1,28 @@
 <?php include '__navigation.php' ?>
+<script>
+$(document).ready( function() {
+
+// Default 2nd list to bottom
+$('#list2').scrollTop($('#list2')[0].scrollHeight);
+
+// Get scrollable height for both lists
+var list1Height = $('#list1')[0].scrollHeight;
+var list2Height = $('#list2')[0].scrollHeight;
+
+// When list 1 is scrolled....
+$('#list1').scroll( function() {
+
+  // Calculate current offset % for list1 (being scrolled by user)
+  // and new offset % for list2 (scroll position adjusted with this script)
+  var list1Offset = ( $(this).scrollTop() + $(this).height() ) / list1Height;
+  var list2Offset = 1 - list1Offset;
+  var list2Position = list2Height * list2Offset;
+
+  // Set new scrolll position for list2
+  $('#list2').scrollTop(list2Position);
+});
+
+});</script>
 
 <section class="fold">
   <div class="fold_black">
@@ -42,21 +66,20 @@
   </div>
 </section>
 
-<!-- <section class="twotale">
-  <div class="block-50">
-    <div class="gutter">
-      <img src="./assets/images/twotales/twotales_01_left01.png" alt="" >
-    </div>
-    <div class="gutter">
+<section class="twotale_flex">
+<div id="list1" class="disable-scrollbars">
+  <div class="gutter">
     <img src="./assets/images/twotales/twotales_01_left01.png" alt="" >
-    </div>
-    <div class="gutter">
-    <img src="./assets/images/twotales/twotales_01_left01.png" alt="" >
-    </div>
   </div>
+  <div class="gutter">
+  <img src="./assets/images/twotales/twotales_01_left01.png" alt="" >
+  </div>
+  <div class="gutter">
+  <img src="./assets/images/twotales/twotales_01_left01.png" alt="" >
+  </div>
+</div>
 
-  <div class="block-50">
-    <div class="panelCon">
+<div id="list2" class="disable-scrollbars">
       <div class="gutter">
           <img src="./assets/images/twotales/twotales_01_right01.png" alt="" >
       </div>
@@ -66,9 +89,8 @@
       <div class="gutter">
           <img src="./assets/images/twotales/twotales_01_right01.png" alt="" >
       </div>
-    </div>
-  </div>
-</section> -->
+</div>
+</section>
 
 <section class="twotales_link">
   <div class="twotales_link_text">
