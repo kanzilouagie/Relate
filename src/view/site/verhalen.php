ec6d40fb18c0194911776155653e937c12001409
Kanzi Louagie <kanzi.louagie@student.howest.be>
2019-06-10 20:07:12 +0200

    nog wat styling

--- src/view/site/verhalen.php
+++ src/view/site/verhalen.php
@@ -5,32 +5,32 @@
   <?php foreach ($verhalen as $verhaal) {
     if($verhaal['published'] == true) { ?>
 
-  <div class="story_box">
-      <div class="box_content">
 
-      <?php if($verhaal['type'] == "video") { ?>
-        <img width="200" src="./assets/videos/stories/<?php echo $verhaal['text_pic']?>" alt="<?php echo $verhaal['text_pic']?>">
-      <?php } ?>
+<div class="story_box">
+      <a class="story_box__link"href="#">
+        <?php if($verhaal['type'] == "video") { ?>
+          <img width="200" src="./assets/videos/stories/<?php echo $verhaal['text_pic']?>" alt="<?php echo $verhaal['text_pic']?>">
+        <?php } ?>
 
-      <?php if($verhaal['type'] == "text") { ?>
-        <img width="200" src="./assets/images/stories/<?php echo $verhaal['text_pic']?>" alt="<?php echo $verhaal['text_pic']?>">
-      <?php } ?>
+        <?php if($verhaal['type'] == "text") { ?>
+          <img width="200" src="./assets/images/stories/<?php echo $verhaal['text_pic']?>" alt="<?php echo $verhaal['text_pic']?>">
+        <?php } ?>
 
-      <?php if($verhaal['type'] == "audio") { ?>
-        <img width="200" src="./assets/audio/stories/<?php echo $verhaal['text_pic']?>" alt="<?php echo $verhaal['text_pic']?>">
-      <?php } ?>
-      </div>
-      <div class="box_info">
-      <div class="box_info__left">
-      <p class="box_info__title"><?php echo $verhaal['story_name'] ?></p>
-      <p class="box_info__relates"><?php echo $verhaal['relates'] ?></p>
-      </div>
-      <div class="box_info__right">
-      <p class="box_info__user"><?php if($verhaal['anonymous'] == 1) { echo "anonymous";} else { echo $verhaal['name'];}?></p>
-      <button>Relate</button>
-      </div>
-      </div>
-  </div>
+        <?php if($verhaal['type'] == "audio") { ?>
+          <img width="200" src="./assets/audio/stories/<?php echo $verhaal['text_pic']?>" alt="<?php echo $verhaal['text_pic']?>">
+        <?php } ?>
+        <div class="box_info">
+          <div class="box_info__left">
+            <p class="box_info__title"><?php echo $verhaal['story_name'] ?></p>
+            <p class="box_info__relates"><?php echo $verhaal['relates'] ?> relates</p>
+          </div>
+            <div class="box_info__right">
+            <p class="box_info__user">by <?php if($verhaal['anonymous'] == 1) { echo "anonymous";} else { echo $verhaal['name'];}?></p>
+            <!-- <button class="box_info__button">Relate</button> -->
+          </div>
+        </div>
+      </a>
+</div>
     <?php }}  ?>
 
   </div>
