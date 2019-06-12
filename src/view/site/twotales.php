2b31ffa5d3559cca760db633030e864240621337
Kanzi Louagie <kanzi.louagie@student.howest.be>
2019-06-13 01:20:21 +0200

    relates pagina is af swa

--- src/view/site/twotales.php
+++ src/view/site/twotales.php
@@ -5,43 +5,48 @@
     <p class="twotale_subtitle">Op het eerste zicht zou je misschien denken date de verhalen in het Opera & Ballet geen
       verband hebben met de verhalen die we dagdagelijks meemakenn maar toch hebben ze betrekking tot elkaar.</p>
   </section>
-  <section class="twotales_container">
-  <div class="twotales_container__item">
-      <a class="twotales_link" href="#">
-        <img src="./assets/images/preview_tale1.png" alt="preview 1">
-        <div class="twotales_container__info">
-          <p class="twotales_title">Macbeth</p>
-          <p class="twotale_relate_title">Cheima</p>
-        </div>
-      </a>
-    </div>
-    <div class="twotales_container__item">
-      <a class="twotales_link" href="#">
-        <img src="./assets/images/preview_tale1.png" alt="preview 1">
-        <div class="twotales_container__info">
-          <p class="twotales_title">Macbeth</p>
-          <p class="twotale_relate_title">Cheima</p>
-        </div>
-      </a>
-    </div>
-    <div class="twotales_container__item">
-      <a class="twotales_link" href="#">
-        <img src="./assets/images/preview_tale1.png" alt="preview 1">
-        <div class="twotales_container__info">
-          <p class="twotales_title">Macbeth</p>
-          <p class="twotale_relate_title">Cheima</p>
-        </div>
-      </a>
-    </div>
-    <div class="twotales_container__item">
-      <a class="twotales_link" href="#">
-        <img src="./assets/images/preview_tale1.png" alt="preview 1">
-        <div class="twotales_container__info">
-          <p class="twotales_title">Macbeth</p>
-          <p class="twotale_relate_title">Cheima</p>
-        </div>
-      </a>
-    </div>
+  <section>
+    <div class="twotales_grid">
+    <div class="twotales_box">
+        <a class="twotales_box__link" href="#">
+          <img src="./assets/images/preview_tale1.png" alt="preview 1">
+          <div class="box_tales">
+            <p class="box_tales__title">Macbeth</p>
+            <hr />
+            <p class="box_tales__relates">Cheima</p>
+          </div>
+        </a>
+      </div>
+      <div class="twotales_box">
+        <a class="twotales_box__link" href="#">
+          <img src="./assets/images/preview_tale1.png" alt="preview 1">
+          <div class="box_tales">
+            <p class="box_tales__title">Macbeth</p>
+            <hr />
+            <p class="box_tales__relates">Cheima</p>
+          </div>
+        </a>
+      </div>
+      <div class="twotales_box">
+        <a class="twotales_box__link" href="#">
+          <img src="./assets/images/preview_tale1.png" alt="preview 1">
+          <div class="box_tales">
+            <p class="box_tales__title">Macbeth</p>
+            <hr />
+            <p class="box_tales__relates">Cheima</p>
+          </div>
+        </a>
+      </div>
+      <div class="twotales_box">
+        <a class="twotales_box__link" href="#">
+          <img src="./assets/images/preview_tale1.png" alt="preview 1">
+          <div class="box_tales">
+            <p class="box_tales__title">Macbeth</p>
+            <hr />
+            <p class="box_tales__relates">Cheima</p>
+          </div>
+        </a>
+      </div>
   </section>
 </main>
 <?php include '__footer.php'?>
