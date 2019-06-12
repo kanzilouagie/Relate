7034d5d009b9be15513751884f6dad85b7e30a4c
XavierAllen <xavier.allen@student.howest.be>
2019-06-13 01:45:44 +0200

    fix stats

--- src/view/site/index.php
+++ src/view/site/index.php
@@ -38,6 +38,16 @@ $('#list1').scroll( function() {
       </div>
     </div>
     <img class="fold_black_img" src="./assets/images/header_Home.png" alt="" >
+
+    <div class="social_logos">
+    <a class="footer_logos_img" href="">
+      <svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13.66 27.33"><defs></defs><title>Asset 13</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M8.54,9.39V6a1.72,1.72,0,0,1,1.71-1.71H12V0H8.54A5.12,5.12,0,0,0,3.42,5.12V9.39H0v4.27H3.42V27.33H8.54V13.66H12l1.7-4.27Z"/></g></g></svg>
+    </a>
+    <a class="footer_logos_img" href="">
+      <svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28.58 23.22"><defs></defs><title>Asset 14</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M28.58,2.75a12.08,12.08,0,0,1-3.38.92A5.76,5.76,0,0,0,27.78.44a11.64,11.64,0,0,1-3.71,1.41,5.89,5.89,0,0,0-10,5.35A16.63,16.63,0,0,1,2,1.07,5.87,5.87,0,0,0,3.79,8.9a5.86,5.86,0,0,1-2.65-.72v.06A5.89,5.89,0,0,0,5.84,14a5.91,5.91,0,0,1-1.54.19,5.15,5.15,0,0,1-1.11-.1,5.92,5.92,0,0,0,5.48,4.08,11.8,11.8,0,0,1-7.27,2.5A10.75,10.75,0,0,1,0,20.59a16.45,16.45,0,0,0,9,2.63c10.78,0,16.67-8.93,16.67-16.67,0-.26,0-.51,0-.76A11.49,11.49,0,0,0,28.58,2.75Z"/></g></g></svg>  </a>
+    <a class="footer_logos_img" href="">
+      <svg width="20px" height="20px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27.33 27.33"><defs></defs><title>Asset 15</title><g id="Layer_2" data-name="Layer 2"><g id="Layer_1-2" data-name="Layer 1"><path class="cls-1" d="M18.79,0H8.54A8.54,8.54,0,0,0,0,8.54V18.79a8.54,8.54,0,0,0,8.54,8.54H18.79a8.55,8.55,0,0,0,8.54-8.54V8.54A8.54,8.54,0,0,0,18.79,0Zm6,18.79a6,6,0,0,1-6,6H8.54a6,6,0,0,1-6-6V8.54a6,6,0,0,1,6-6H18.79a6,6,0,0,1,6,6Z"/><path class="cls-1" d="M13.66,6.83a6.84,6.84,0,1,0,6.84,6.83A6.83,6.83,0,0,0,13.66,6.83Zm0,11.1a4.27,4.27,0,1,1,4.27-4.27A4.27,4.27,0,0,1,13.66,17.93Z"/><circle class="cls-1" cx="21.01" cy="6.32" r="0.91"/></g></g></svg>  </a>
+  </div>
   </div>
 
   <div class="fold_red">
@@ -64,11 +74,21 @@ $('#list1').scroll( function() {
    <img class="stats_image_red" src="./assets/images/stats_image.png" alt="" >
   </div>
   <div class="stats_text">
-    <p>3 Upload methodes</p>
+    <div>
+      <p>Upload methodes:</p>
+      <p class="stats_aantal">3</p>
+    </div>
     <div class="vr">&nbsp;</div>
-    <p>4 Two-tales</p>
+    <div>
+      <p>Relates:</p>
+      <p class="stats_aantal">4</p>
+    </div>
     <div class="vr">&nbsp;</div>
-    <p><?php echo count($verhalen);?> Eigen Verhalen</p>
+    <div>
+      <p>Eigen Verhalen:</p>
+      <p class="stats_aantal"><?php echo count($verhalen);?></p>
+    </div>
+
   </div>
 </section>
 
